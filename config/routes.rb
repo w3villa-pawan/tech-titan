bc2a768b6618d69b76ee1df16907c3a583fae7a3
w3villa-pawan <pawan.sharma@w3villa.com>
2024-08-30 15:30:39 +0530

    Add home controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'home#index'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -11,5 +12,4 @@ Rails.application.routes.draw do
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
   # Defines the root path route ("/")
-  root "#index"
 end
