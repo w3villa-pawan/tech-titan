423a5daf4b8a779a7057da76f8dfe5c5bd6fb36d
w3villa-pawan <pawan.sharma@w3villa.com>
2024-08-30 17:23:56 +0530

    added omniauth

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -17,9 +17,31 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   # end
 
   # GET|POST /users/auth/twitter/callback
-  # def failure
-  #   super
-  # end
+ 
+
+  def google_oauth2
+    user = User.from_omniauth(auth)
+    if user.present?
+      sign_out_all_scopes
+      flash[:success] = t 'devise_omniauth_callbacks.success', kind: 'Google'
+      sign_in_and_redirect user, event: :authentication
+    else
+      flash[:alert] = 
+        t 'devise_omniauth_callbacks.failure', kind: 'Google', reason: "#{auth.info.email} is not authorized"
+      redirect_to_new_user_session_path
+    end
+  end
+
+  def failure
+    super
+  end
+
+  
+  private
+  
+  def auth
+    auth ||= request.env['omniauth.auth']
+  end
 
   # protected
 
