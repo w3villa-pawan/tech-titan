993d268e41316caef465adfed2e26e884fe45517
w3villa-rahul-yadav <rahul.yadav@w3villa.com>
2024-08-31 14:49:02 +0530

    "Updated application stylesheet, registrations controller, home and registrations views, and routes configuration"

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'home#index'
   devise_for :users, controllers: {
+    registrations: 'users/registrations',
     sessions: 'users/sessions',
     omniauth_callbacks: 'users/omniauth_callbacks'
   }
